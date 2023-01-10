c45b153d8b007278cade9e3a6b38eec454841fcb
mhkim <xowldidi@naver.com>
2023-01-10 14:17:56 +0900

    Initial Commit

---
+++ objcHelloWorld/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  objcHelloWorld
+//
+//  Created by IMStudio on 2023/01/10.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
